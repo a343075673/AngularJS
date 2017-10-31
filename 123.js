391c729e20549a2704d6488468f329bb5e80883f
tongxin zhang <zhangtx1021@gmail.com>
2017-10-31 16:07:42 -0400

    Add files via upload

---
+++ 123.js
@@ -0,0 +1,17 @@
+var k =[
+    {type:"sitting"},
+    {type:"standing"},
+    {type:"standing"},
+    {type:"sitting"},
+    {type:"sitting"}
+]
+
+
+var desktype = k.reduce(function (a,b) {
+    return
+
+})
+
+
+
+console.log(desktype)
